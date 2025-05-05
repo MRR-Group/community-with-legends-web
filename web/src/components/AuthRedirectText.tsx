68985cd7ae23e2598af7636dc92bef9482c3743b
Oliwia Charyszczak <oliwia.charyszczak@studenci.collegiumwitelona.pl>
2025-05-06 01:02:01 +0200

    Refactor component

--- web/src/components/AuthRedirectText.tsx
+++ web/src/components/AuthRedirectText.tsx
@@ -4,9 +4,9 @@ interface AuthRedirectTextProps{
 }
 export default function AuthRedirectText({message, actionText}: AuthRedirectTextProps) {
   return (
-    <div className='flex items-center'>
-      <div className='flex-1 text-xs'>{message}</div>
-      <div className='text-center'>
+    <div className='flex items-center justify-between'>
+      <div className='flex-1 text-xs text-wrap max-w-32'>{message}</div>
+      <div className='text-end ml-auto'>
         <div className='text-xs text-primary'>Click here</div>
         <div className='text-xs'>{actionText}</div>
       </div>
