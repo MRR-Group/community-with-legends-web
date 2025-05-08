b4d90bac40403e55d26bb3afbc20b2f7896bd5bb
Oliwia Charyszczak <oliwia.charyszczak@studenci.collegiumwitelona.pl>
2025-05-08 22:15:01 +0200

    Add router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RegisterPage from './RegisterPage.tsx'
+import LoginPage from "./LoginPage.tsx";
+import ForgotPasswordPage from "./ForgotPasswordPage.tsx";
+import ResetPasswordPage from "./ResetPasswordPage.tsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: LoginPage,
+  },
+  {
+    path: "/register",
+    Component: RegisterPage,
+  },
+  {
+    path: "/forgot-password",
+    Component: ForgotPasswordPage,
+  },
+  {
+    path: "/reset-password",
+    Component: ResetPasswordPage,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RegisterPage />
+    <RouterProvider router={router} />
   </StrictMode>,
+
 )
