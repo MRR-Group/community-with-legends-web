b63f61f5e46cb4a69107948e8009bec09bec7e8f
Oliwia-Charyszczak <oliwia.charyszczak@studenci.collegiumwitelona.pl>
2025-05-28 19:01:02 +0000

    Add tags to the post creation

--- web/src/components/CreatePost.tsx
+++ web/src/components/CreatePost.tsx
@@ -4,6 +4,7 @@ import Input from "./Input.tsx";
 import {SelectFetch} from "react-select-fetch";
 import {useState} from "react";
 import GameDto from "../../../core/src/dto/gameDto.ts";
+import TagDto from "../../../core/src/dto/tagDto.ts";
 
 export interface CreatePostForm {
     content: string,
@@ -12,6 +13,7 @@ export interface CreatePostForm {
 export interface SubmitProp {
     content: string,
     gameId?: number,
+    tags: number[],
 }
 
 interface CreatePostFormProps {
@@ -27,16 +29,19 @@ interface SelectElement {
 export default function CreatePost({onSubmit, errors}: CreatePostFormProps) {
     const { register, handleSubmit, reset } = useForm<PostModel>();
     const [selectedGame, setGame] = useState<SelectElement|null>(null);
+    const [selectedTags, setSelectedTags] = useState<readonly SelectElement[]>([]);
 
     const handleSubmitClick: SubmitHandler<CreatePostForm> = async(data) => {
-        await onSubmit({content: data.content, gameId: selectedGame?.value});
+        await onSubmit({content: data.content, gameId: selectedGame?.value, tags: selectedTags.map((tag) => tag.value)});
         reset();
+        setGame(null);
+        setSelectedTags([]);
     };
 
     return(
         <div className='p-0.5 bg-gradient-to-b from-[#1E9AC8] to-[#8E2CFE] rounded-[10px] max-w-96 md:max-w-128'>
             <div className='flex flex-col gap-4 bg-background px-5 rounded-lg max-w-96 md:max-w-128 relative box-border'>
-                <form onSubmit={handleSubmit(handleSubmitClick)} className='flex gap-4 flex-col bg-background px-5 rounded-lg min-w-80'>
+                <form onSubmit={handleSubmit(handleSubmitClick)} className='flex gap-4 flex-col bg-background px-5 rounded-lg xs:min-w-80'>
                     <div className='flex flex-col gap-2'>
                         <Input
                             register={register}
@@ -51,11 +56,13 @@ export default function CreatePost({onSubmit, errors}: CreatePostFormProps) {
                             styles={{
                                 control: (styles) => ({...styles, backgroundColor: '#212023', color: '#FFF', border: 'none', boxShadow: 'none'}),
                                 option: (styles) => ({...styles, backgroundColor: '#212023', color: '#FFF'}),
-                                placeholder: (styles) => ({...styles, color: '#B9B9BA'}),
+                                placeholder: (styles) => ({...styles, color: '#8e8f90' , fontSize: '14px', fontWeight: '400'}),
                                 singleValue: (styles) => ({...styles, color: '#FFF'}),
                                 input: (styles) => ({...styles, color: '#FFF'}),
+                                loadingMessage: (styles) => ({...styles, backgroundColor: '#212023', color: '#FFF'}),
                             }}
                             className='text-text bg-background'
+                            placeholder='Select game'
                             url="/api/games/search"
                             value={selectedGame}
                             onChange={setGame}
@@ -69,6 +76,32 @@ export default function CreatePost({onSubmit, errors}: CreatePostFormProps) {
                             queryParams={{
                                 limit: 10
                             }}
+                        />
+
+                        <SelectFetch
+                            styles={{
+                                control: (styles) => ({...styles, backgroundColor: '#212023', color: '#FFF', border: 'none', boxShadow: 'none'}),
+                                option: (styles) => ({...styles, backgroundColor: '#212023', color: '#FFF'}),
+                                placeholder: (styles) => ({...styles, color: '#8e8f90', fontSize: '14px', fontWeight: '400'}),
+                                singleValue: (styles) => ({...styles, color: '#FFF'}),
+                                input: (styles) => ({...styles, color: '#FFF'}),
+                                loadingMessage: (styles) => ({...styles, backgroundColor: '#212023', color: '#FFF'}),
+                                multiValue: (styles) => ({...styles, backgroundColor: '#8E2CFE'}),
+                                multiValueLabel: (styles) => ({...styles, color: '#FFF'}),
+                            }}
+                            isMulti={true}
+                            className='text-text bg-background'
+                            placeholder='Select tags'
+                            url="/api/tags/search"
+                            value={selectedTags}
+                            onChange={setSelectedTags}
+                            mapResponse={(response: any) => ({
+                                options: response.data.map((tag: TagDto) => ({
+                                    value: tag.id,
+                                    label: tag.name,
+                                })),
+                                hasMore: false,
+                            })}
                         />
 
                     </div>
--- web/src/components/NavButton.tsx
+++ web/src/components/NavButton.tsx
@@ -8,7 +8,7 @@ interface NavButtonProps {
 export default function NavButton({active, text, icon}: NavButtonProps) {
   return (
     <li
-      className={cn('flex flex-row gap-1 items-center h-10 transition-colors delay-100 hover:text-primary group cursor-pointer ml-3 xs:ml-10', active ? "text-primary" : "text-text")}>
+      className={cn('flex flex-row gap-1 items-center h-10 transition-colors delay-100 hover:text-primary group cursor-pointer ml-10', active ? "text-primary" : "text-text")}>
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={cn('transition-colors delay-100 group-hover:stroke-primary', active ? "stroke-primary" : "stroke-text")}>
         <path
           d={icon} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
